33d919b92470a65df043137e2b7e717d5025f2f0
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-12 14:33:59 +0900

    Add Occupation enum in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  enum occupation: { planner: 0, engineer: 1, designer: 2 }}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
