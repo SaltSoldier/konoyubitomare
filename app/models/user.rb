9621f4556c763e64a433cb9b23f43f56e84d221f
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-12 14:37:28 +0900

    Add Mochivation enum in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
-  enum occupation: { planner: 0, engineer: 1, designer: 2 }}
+  enum occupation: { planner: 0, engineer: 1, designer: 2 }
+  enum mochivation: { no_mochivation: 0, little_no_mochivation: 1, normal_mochivation: 2, pretty_mochivation: 3, max_mochivation: 4 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
