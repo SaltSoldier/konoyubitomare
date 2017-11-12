578918b1ffe7fe1617c37ef4182d67854a9db167
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-12 20:26:49 +0900

    [WIP]ログアウト機能が実装できない

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   root 'home#index'
-  get 'home/index'
-
-  get 'home/authentication'
+  get 'index' => 'home#index'
+  get 'authentication' => 'home#authentication'
+  delete 'logout' => 'devise/sessions#destroy'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
