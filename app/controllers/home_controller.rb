bf2855e9f398405c2c974636b05382d816020c1c
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-12 15:55:40 +0900

    Add input form for mochivation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user! ,only: :authentication
+  before_action :authenticate_user!
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
 
   get 'home/authentication'
