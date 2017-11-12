fc7c2f85ad1ddea942748fae5118f74c5f8f78c1
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-12 15:41:14 +0900

    Add input form for Occupation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me, :graduation_year, :occupation, :mochivation) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:username, :email, :password, :password_confirmation, :remember_me)
+      end
+      devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+        user_params.permit(:login, :username, :email, :password, :remember_me, :graduation_year, :occupation, :mochivation)
+      end
+      devise_parameter_sanitizer.permit(:account_update) do |user_params|
+        user_params.permit(:username, :email, :password, :password_confirmation, :current_password)
+      end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109115028) do
+ActiveRecord::Schema.define(version: 20171110074108) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20171109115028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "graduation_year"
+    t.integer "occupation"
+    t.integer "mochivation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
