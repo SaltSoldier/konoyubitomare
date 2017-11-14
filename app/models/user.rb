a63b765f97af06730edf18d8fde4f449fd02a3d7
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-14 20:33:12 +0900

    Add has_many :project in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_many :projects
+
   enum occupation: { planner: 0, engineer: 1, designer: 2 }
   enum mochivation: { no_mochivation: 0, little_no_mochivation: 1, normal_mochivation: 2, pretty_mochivation: 3, max_mochivation: 4 }
 
