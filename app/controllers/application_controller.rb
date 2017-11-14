d56755ca36a9c36f76824cbe7cdcdd5d5ce92db2
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-14 17:35:07 +0900

    Edit permitted_parameters in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-        user_params.permit(:username, :email, :password, :password_confirmation, :remember_me)
+        user_params.permit(:login, :username, :email, :password, :remember_me, :graduation_year, :occupation, :mochivation)
       end
       devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-        user_params.permit(:login, :username, :email, :password, :remember_me, :graduation_year, :occupation, :mochivation)
+        user_params.permit(:email, :password, :remember_me)
       end
       devise_parameter_sanitizer.permit(:account_update) do |user_params|
         user_params.permit(:username, :email, :password, :password_confirmation, :current_password)
