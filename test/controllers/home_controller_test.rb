2ce8b6fe289d2d2a44bd645d6f2bc7db13ac8011
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-14 16:44:32 +0900

    mini test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get index
+    get index_path
     assert_response :success
   end
 
   test "should get authentication" do
-    get authentication
+    get authentication_path
     assert_response :success
   end
 
