937ebbf20c845ce73e90093f2e9f8195d08e2b38
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-14 20:40:23 +0900

    run 'bundle exec rails g controller projects' & Add route project in user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'index' => 'home#index'
   get 'authentication' => 'home#authentication'
 
+  resources :users do
+    resources :projects
+  end
+
   devise_for :users 
   devise_scope :user do
     get 'sign_in' => 'devise/sessions#new'
